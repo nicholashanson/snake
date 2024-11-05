e45a027d633623a1ee5f0f14b17d9284554094ee
nicholashanson <nicholashanson27@outlook.com>
2024-11-05 09:07:45 +0800

    Update Apple.cpp

--- src/Apple.cpp
+++ src/Apple.cpp
@@ -5,10 +5,12 @@ Apple::Apple(){}
 Apple::~Apple(){}
 
 // getters
-auto Apple::get_position() const -> square_t{return position;}
+auto Apple::get_position() const -> square_t
+{return position;}
 
 // setters
-auto Apple::set_position(const square_t pos) -> void{position = pos;}
+auto Apple::set_position(const square_t pos) -> void
+{position = pos;}
 
 auto Apple::set_position(const int x, const int y) -> void
 {set_position( { x, y } );}
